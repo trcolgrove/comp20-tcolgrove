b2addee3d5c616227ac1f440442fcc714f3c97c3
trcolgrove <trcolgrove@gmail.com>
2015-03-03 14:03:08 -0500

    implemented script to draw ms. pacman board on campus

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,7 +1,11 @@
 // Replace this with your code...
 function init(){
-  canvas = document.getElementById('game_canvas');
-  img = new Image('pacman10-hp-sprite.png');
+  canvas = document.getElementById("game_canvas");
   ctx = canvas.getContext('2d');
-  ctx.drawImage(img, 0, 0);
+  img = new Image();
+  img.src = "pacman10-hp-sprite.png";
+  img.onload = function(){
+    ctx.drawImage(img, 322, 2, 465, 136, 0, 0, 465, 136 );
+    ctx.drawImage(img, 78, 20, 22, 22, 32, 28, 22, 22);
+  }
 }
