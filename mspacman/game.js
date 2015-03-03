d88ab3098e3fd3291cda661ab582eba6e47a2139
trcolgrove <trcolgrove@gmail.com>
2015-03-03 12:45:21 -0500

    starting lab 5

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,7 @@
 // Replace this with your code...
+function init(){
+  canvas = document.getElementById('game_canvas');
+  img = new Image('pacman10-hp-sprite.png');
+  ctx = canvas.getContext('2d');
+  ctx.drawImage(img, 0, 0);
+}
