932549e67866dabbe94a20f7e4f9bf9f0a5968e4
trcolgrove <trcolgrove@gmail.com>
2015-03-10 15:14:36 -0400

    testing parse function

--- messages/lab.js
+++ messages/lab.js
@@ -1,18 +1,18 @@
 // Your JavaScript goes here...
-function reqListener () {
-  console.log(this.responseText);
-}
-
 
 function parse(){
   var out = ""
   var xmlhttp = new XMLHttpRequest();
   var url = "data.json";
 
+
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+
   xmlhttp.onreadystatechange = function(){
   for(var i = 0; i < arr.length; i++){
+        var arr = JSON.parse(xmlhttp.responseText);
         out += arr[i].content;
-        myFunction(myArr);
         console.log("doooooodoodoo")
         document.getElementById("messages").innerHTML = out;
       }
