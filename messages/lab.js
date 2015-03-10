d1536be4851981e540021034f26fb6192baba44d
trcolgrove <trcolgrove@gmail.com>
2015-03-10 14:14:30 -0400

    testing javascript

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,14 @@
 // Your JavaScript goes here...
+function reqListener () {
+  console.log(this.responseText);
+}
+
+function parse(){
+  var req = new XMLHttpRequest();
+  var url = "data.json"
+  req.onload = reqListener;
+  req.open("get", url, true);
+  req.send();
+  document.getElementByID("messages").innerHTML
+
+}
