41ce9a7282e4c2c6964f5a4e7510111fd5029850
trcolgrove <trcolgrove@gmail.com>
2015-03-10 15:10:20 -0400

    testing parse function

--- messages/lab.js
+++ messages/lab.js
@@ -10,14 +10,12 @@ function parse(){
   var url = "data.json";
 
   xmlhttp.onreadystatechange = function(){
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-          var myArr = JSON.parse(xmlhttp.responseText);
-          parse(myArr);
-    }
   for(var i = 0; i < arr.length; i++){
         out += arr[i].content;
         myFunction(myArr);
+        req.onload = reqListener;
+        document.getElementById("messages").innerHTML = out;
+  
   }
-  req.onload = reqListener;
-  document.getElementById("messages").innerHTML = out;
+  }
 }
