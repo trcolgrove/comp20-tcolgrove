07c78ee22cb4c81e21520644b41bf0876a782f5e
trcolgrove <trcolgrove@gmail.com>
2015-03-10 15:49:49 -0400

    fixed messages doubling

--- messages/lab.js
+++ messages/lab.js
@@ -1,22 +1,21 @@
 // Your JavaScript goes here...
 
 function parse(){
-  var out = ""
   var xmlhttp = new XMLHttpRequest();
   var url = "data.json";
-
-
   xmlhttp.open("GET", url, true);
   xmlhttp.send();
 
     xmlhttp.onreadystatechange = function(){
-    toUpdate = document.getElementById("messages");
-    var arr = JSON.parse(xmlhttp.responseText);
-    for(var i = 0; i < arr.length; i++){
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+      toUpdate = document.getElementById("messages");
+      var arr = JSON.parse(xmlhttp.responseText);
+      for(var i = 0; i < arr.length; i++){
         username = arr[i].username;
         content = arr[i].content;
         toUpdate.innerHTML += '<p class="message"> <span class="content">' + content + '</span> <span class="username">' + username+
         '</span> </p>'
-    }
+      }
+    }  
   }
 }
