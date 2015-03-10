348dd4020493baddf89aa5da8a1872aa994c693d
trcolgrove <trcolgrove@gmail.com>
2015-03-10 15:09:00 -0400

    testing parse function

--- messages/lab.js
+++ messages/lab.js
@@ -3,18 +3,17 @@ function reqListener () {
   console.log(this.responseText);
 }
 
-var xmlhttp = new XMLHttpRequest();
-var url = "data.json";
 
-xmlhttp.onreadystatechange = function(){
-  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        var myArr = JSON.parse(xmlhttp.responseText);
-        parse(myArr);
-  }
-}
-
-function parse(arr){
+function parse(){
   var out = ""
+  var xmlhttp = new XMLHttpRequest();
+  var url = "data.json";
+
+  xmlhttp.onreadystatechange = function(){
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+          var myArr = JSON.parse(xmlhttp.responseText);
+          parse(myArr);
+    }
   for(var i = 0; i < arr.length; i++){
         out += arr[i].content;
         myFunction(myArr);
